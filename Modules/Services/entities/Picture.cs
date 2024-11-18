329ee569c9f433ab22d2d6f478722c97325747a5
FGaye <fg22126029@utg.edu.gm>
2024-11-18 20:54:08 +0000

    added attributes to the picture entity

--- Modules/Services/entities/Picture.cs
+++ Modules/Services/entities/Picture.cs
@@ -2,5 +2,7 @@ namespace ServiceApp.Modules.Services.entities;
 
 public class Picture
 {
-    
+    public int Id {get; set; }
+    public string Url {get; set;}
+    public string Description {get; set;}
 }
